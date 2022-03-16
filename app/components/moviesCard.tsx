89ebb920f277b90b3827dc8a848c8f11bd918adc
Harun Doğdu <harundogdu06@gmail.com>
2022-03-16 21:30:52 +0300

    movies card designed

--- app/components/moviesCard.tsx
+++ app/components/moviesCard.tsx
@@ -1,15 +1,23 @@
-import { IMoviesProps } from "~/types/movies";
+import {IMoviesProps} from "~/types/movies";
 
-function MoviesCard({ movie }: { movie: IMoviesProps }) {
-  const BASE_IMAGE_URL = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
-  return (
-    <div className="text-center">
-      <h1 className="font-bold">{movie.title}</h1>
-      <div>
-        <img src={`${BASE_IMAGE_URL}/${movie.poster_path}`} alt={movie.title} />
-      </div>
-    </div>
-  );
+function MoviesCard({movie}: { movie: IMoviesProps }) {
+    const BASE_IMAGE_URL = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
+    return (
+        <div className="text-center m-2 rounded-lg cursor-pointer group hover:scale-105 transition-transform duration-300">
+            <div className="relative">
+                <img
+                    src={`${BASE_IMAGE_URL}/${movie.poster_path}`}
+                    alt={movie.title}
+                    className="rounded-lg"
+                />
+                <h1
+                    className="group-hover:flex font-bold text-sm absolute bottom-0 left-0 h-10 w-full bg-black bg-opacity-40 text-white hidden justify-center items-center px-4 py-5 rounded-t"
+                >
+                    {movie.title}
+                </h1>
+            </div>
+        </div>
+    );
 }
 
 export default MoviesCard;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
   return (
     <div className="flex flex-col justify-center items-center flex-1 py-8">
       <h1 className="text-3xl font-bold">Popular Movies</h1>
-      <div className="flex flex-wrap space-y-4 space-x-4">
+      <div className="grid grid-cols-5 mt-6">
         {movies.map((movie) => (
           <MoviesCard movie={movie} key={movie.id} />
         ))}
