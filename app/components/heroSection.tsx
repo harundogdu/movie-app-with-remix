0a101582606ff9ff938f400409005b238e43b576
Harun Doğdu <harundogdu06@gmail.com>
2022-03-18 02:00:06 +0300

    herosection component created

---
+++ app/components/heroSection.tsx
@@ -0,0 +1,14 @@
+function HeroSection({img, title}: { img: string, title: string }) {
+    return (
+        <div className="w-full h-96 shadow rounded relative">
+            <img src={img} alt="Hero Section" className="w-full h-full object-cover shadow rounded"/>
+            <div className="bg-black opacity-75 rounded-t-lg absolute bottom-0 right-0">
+                <div className="w-full h-full flex flex-col items-end justify-end p-4">
+                    <h1 className="text-white text-center text-3xl font-bold">{title}</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection;
