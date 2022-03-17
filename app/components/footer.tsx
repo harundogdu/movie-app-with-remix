d4e0aadbb4f7dc4557a714df33b52f25f6b6264e
Harun Doğdu <harundogdu06@gmail.com>
2022-03-18 01:59:55 +0300

    header and footer components changed

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,19 @@
 const Footer = () => {
-  return (
-    <footer className="bg-gray-200 p-5 text-center">
-      <h1>Burası Footer</h1>
-    </footer>
-  );
+    return (
+        <footer
+            className="bg-gradient-to-b from-primary to-secondary p-5 text-center text-light flex items-center justify-center">
+            <p className="mr-4">
+                copyright &copy; {new Date().getFullYear()} - powered by |
+            </p>
+            <div className="flex items-center justify-center font-bold">
+                <a href="https://harundogdu.com" rel="noreferrer" target="_blank"
+                   className="hover:!text-brandYellow transition-colors">Harun Doğdu</a>
+                <span className="mx-4">&</span>
+                <a href="https://github.com/mduldul" rel="noreferrer" target="_blank"
+                   className="hover:!text-brandYellow transition-colors">Mert Can Düldül</a>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,30 @@
+import React from "react";
+import {IoMdMoon} from "react-icons/io";
+import {MdOutlineLightMode} from "react-icons/md";
+
 const Header = () => {
-  return (
-    <header className="bg-gray-200 p-5 text-center">
-      <h1>Burası Header</h1>
-    </header>
-  );
+    const [theme, setTheme] = React.useState(false);
+
+    const handleChangeTheme = () => {
+        setTheme(!theme);
+    };
+
+    return (
+        <header className="bg-gradient-to-b from-primary to-secondary px-4 py-2 text-center flex items-center justify-between">
+            <button className="" onClick={handleChangeTheme}>
+                {
+                    theme ? <IoMdMoon size={24} className="text-light"/> : <MdOutlineLightMode size={24} className="text-brandYellow"/>
+                }
+            </button>
+            <div className="flex flex-1 w-full">
+                <input
+                    type="text"
+                    placeholder="Search movie!"
+                    className="bg-white focus:shadow-outline border border-gray-300 rounded-md py-2 px-4 block appearance-none leading-normal w-1/4 ml-auto focus:w-1/2 transition-all duration-300 outline-none text-gray-400"
+                />
+            </div>
+        </header>
+    );
 };
 
 export default Header;
