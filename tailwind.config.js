60a33de770987a76cccd9e2089138fb84d66e8f4
Harun Doğdu <harundogdu06@gmail.com>
2022-03-18 01:59:13 +0300

    theme colors changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                brand: "#eaeaea",
+                primary: "#21325E",
+                secondary: "#3E497A",
+                light: "#F0F0F0",
+                brandYellow: "#F1D00A",
+            }
+        },
+    },
+    plugins: [],
 }
