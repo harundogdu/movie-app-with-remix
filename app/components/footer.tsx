5edbb6121263749dcf9772150d36ff4379e6b680
Harun Doğdu <harundogdu06@gmail.com>
2022-03-19 00:45:09 +0300

    changes in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,16 +1,45 @@
+import {AiOutlineGithub} from "react-icons/ai";
+import {NavLink} from "remix";
+import Logo from "~/assets/logo.png";
+
 const Footer = () => {
     return (
-        <footer
-            className="bg-gradient-to-b from-primary to-secondary p-5 text-center text-light flex items-center justify-center">
-            <p className="mr-4">
-                copyright &copy; {new Date().getFullYear()} - powered by |
-            </p>
-            <div className="flex items-center justify-center font-bold">
-                <a href="https://harundogdu.com" rel="noreferrer" target="_blank"
-                   className="hover:!text-brandYellow transition-colors">Harun Doğdu</a>
-                <span className="mx-4">&</span>
-                <a href="https://github.com/mduldul" rel="noreferrer" target="_blank"
-                   className="hover:!text-brandYellow transition-colors">Mert Can Düldül</a>
+        <footer className="text-gray-600 body-font">
+            <div className="container px-5 py-8 mx-auto flex r sm:flex-row flex-col">
+                <NavLink to="/" className="w-48 mr-auto relative">
+                    <img src={Logo} alt="Sidebar" className="w-full"/>
+                    <small className="absolute bottom-0 left-1/2 -translate-x-1/2 text-xs text-gray-100">
+                        Keyfime Göre
+                    </small>
+                </NavLink>
+                <div className="flex items-center">
+                    <p className="mr-4"> copyright &copy; {new Date().getFullYear()} - powered by | Harun Doğdu & Mert
+                        Can Düldül </p>
+                    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+                         <a
+                             href="https://github.com/harundogdu"
+                             target="_blank"
+                             rel="noreferrer"
+                             title="Harun Doğdu"
+                         >
+                           <AiOutlineGithub
+                               className="text-gray-600 hover:text-gray-300 transition-colors mx-1"
+                               size="2em"
+                           />
+                        </a>
+                        <a
+                            href="https://github.com/mduldul"
+                            target="_blank"
+                            rel="noreferrer"
+                            title="Mert Can Düldül"
+                        >
+                           <AiOutlineGithub
+                               className="text-gray-600 hover:text-gray-300 transition-colors mx-1"
+                               size="2em"
+                           />
+                        </a>
+                    </span>
+                </div>
             </div>
         </footer>
     );
