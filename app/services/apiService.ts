9846e23bdb5ba5125b281efc41c2a8213be20c79
Harun Doğdu <harundogdu06@gmail.com>
2022-03-19 00:46:12 +0300

    youtube trailer added to project

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -19,3 +19,8 @@ export const getMovie = async (movieId : string) => {
     const response = await apiService.get(`/movie/${movieId}?api_key=${API_KEY}`);
     return response.data;
 }
+
+export const getMovieVideos = async (movieId : string) => {
+    const response = await apiService.get(`/movie/${movieId}/videos?api_key=${API_KEY}`);
+    return response.data;
+}
---
+++ app/types/videos.ts
@@ -0,0 +1,17 @@
+export interface Result {
+    iso_639_1: string;
+    iso_3166_1: string;
+    name: string;
+    key: string;
+    site: string;
+    size: number;
+    type: string;
+    official: boolean;
+    published_at: Date;
+    id: string;
+}
+
+export interface IVideoProps {
+    id: number;
+    results: Result[];
+}
