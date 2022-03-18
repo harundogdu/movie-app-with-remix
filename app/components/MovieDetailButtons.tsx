0ee35366dfd4a6c68198bb15c5c06a4063a5931a
Harun Doğdu <harundogdu06@gmail.com>
2022-03-18 16:11:36 +0300

    button component created

---
+++ app/components/MovieDetailButtons.tsx
@@ -0,0 +1,20 @@
+import {HiExternalLink} from "react-icons/hi";
+
+interface IMovieDetailButtonsProps {
+    title: string;
+    path: string;
+}
+
+export default function MovieDetailButtons({title, path}: IMovieDetailButtonsProps) {
+    return (
+        <button
+            className="bg-white text-primary font-semibold py-2 px-4 rounded-lg hover:bg-brandYellow transition-colors duration-300 flex items-center space-x-2">
+            <a href={path}
+               target="_blank"
+               rel="noopener noreferrer">
+                {title}
+            </a>
+            <HiExternalLink/>
+        </button>
+    );
+}
