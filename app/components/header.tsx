b5b727a1415ee5c628f6c10ac2b85bb22ad34b3a
Harun Doğdu <harundogdu06@gmail.com>
2022-03-24 18:17:44 +0300

    dark mode & light mode created

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,32 +1,62 @@
 import React from "react";
-import { IoMdMoon } from "react-icons/io";
-import { MdOutlineLightMode } from "react-icons/md";
+import { BsFillMoonFill } from "react-icons/bs";
+import { Form, useSubmit } from "remix";
 
 const Header = () => {
-  const [darkMode, setDarkMode] = React.useState(false);
+	const submit = useSubmit();
 
-  const toggleTheme = () => {
-    setDarkMode(!darkMode);
-  };
+	const [darkMode, setDarkMode] = React.useState(false);
+	const [searchQuery, setSearchQuery] = React.useState("");
 
-  return (
-    <header className="bg-gradient-to-b from-primary to-secondary px-8 py-2 text-center flex items-center justify-between">
-      <button className="" onClick={toggleTheme}>
-        {darkMode ? (
-          <IoMdMoon size={24} className="text-light" />
-        ) : (
-          <MdOutlineLightMode size={24} className="text-brandYellow" />
-        )}
-      </button>
-      <div className="flex flex-1 w-full">
-        <input
-          type="text"
-          placeholder="Search movie!"
-          className="bg-white focus:shadow-outline border border-gray-300  py-2 px-4 block appearance-none leading-normal w-1/4 ml-auto focus:w-1/2 transition-all duration-300 outline-none text-gray-400"
-        />
-      </div>
-    </header>
-  );
+	const toggleTheme = () => {
+		localStorage.setItem("dark", JSON.stringify(!darkMode));
+		setDarkMode(!darkMode);
+	};
+
+	const handleSearchQuerySubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		setSearchQuery("");
+		submit(null, { method: "get", action: `/search/${searchQuery}` });
+	};
+
+	React.useEffect(() => {
+		const root = document.documentElement;
+
+		if (darkMode) {
+			root.classList.add("dark");
+		} else {
+			root.classList.remove("dark");
+		}
+
+	}, [darkMode]);
+
+	React.useEffect(() => {
+		const localMode = JSON.parse(localStorage.getItem("dark") || "false");
+
+		setDarkMode(localMode);
+	}, []);
+
+	return (
+		<header className="bg-gradient-to-b dark:from-primary dark:to-secondary px-8 py-2 text-center flex items-center justify-between">
+			<div>
+				<button className="outline-none" onClick={toggleTheme}>
+					<BsFillMoonFill
+						size={24}
+						className={`${darkMode ? "text-brandYellow" : "text-primary"}`}
+					/>
+				</button>
+			</div>
+			<Form className="flex flex-1 w-full" onSubmit={handleSearchQuerySubmit}>
+				<input
+					type="text"
+					placeholder="Search movie!"
+					className="dark:bg-white bg-gray-50 border-gray-800  focus:shadow-outline border dark:border-gray-300  py-2 px-4 block appearance-none leading-normal w-1/4 ml-auto focus:w-1/2 transition-all duration-300 outline-none dark:text-gray-400 text-primary"
+					value={searchQuery}
+					onChange={(e) => setSearchQuery(e.target.value)}
+				/>
+			</Form>
+		</header>
+	);
 };
 
 export default Header;
