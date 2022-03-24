e084d5f99dfdceaf863e34e11c9a9099295d7e97
Harun Doğdu <harundogdu06@gmail.com>
2022-03-24 18:16:28 +0300

    menu item items active event added

--- app/components/menuItem.tsx
+++ app/components/menuItem.tsx
@@ -15,13 +15,18 @@ interface MenuItemLinks {
 function MenuItem(props: MenuItemProps) {
   return (
     <>
-      <small className="text-sm text-light my-3">{props.title}</small>
-      <div className="flex flex-col text-lg px-3 space-y-3">
+      <small className="text-sm dark:text-light my-3">{props.title}</small>
+      <div className="flex flex-col text-lg px-2 space-y-1">
         {props.links.map((link, index) => (
           <NavLink
             to={link.path}
             key={link.id + "_" + index}
-            className="flex items-center text-xl"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white bg-brand text-white dark:bg-red-600"
+                : "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white"
+            }
+
           >
             {link.icon}
             <span className="ml-4">{link.title}</span>
