85c102d37046f75373673161ab4493cc04b6a24b
Harun Doğdu <harundogdu06@gmail.com>
2022-03-26 19:48:21 +0300

    responsive design operations done

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { BsFillMoonFill } from "react-icons/bs";
 import { Form, useSubmit } from "remix";
+import { BsFillMoonFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
 	const submit = useSubmit();
@@ -18,6 +19,11 @@ const Header = () => {
 		setSearchQuery("");
 		submit(null, { method: "get", action: `/search/${searchQuery}` });
 	};
+
+	const handleSidebarVisibleState = () => {
+		document.querySelector("#sidebar")?.classList.toggle("hidden");
+		document.querySelector("#sidebar")?.classList.toggle("flex");
+	}
 
 	React.useEffect(() => {
 		const root = document.documentElement;
@@ -37,24 +43,32 @@ const Header = () => {
 	}, []);
 
 	return (
-		<header className="bg-gradient-to-b dark:from-primary dark:to-secondary px-8 py-2 text-center flex items-center justify-between">
-			<div>
-				<button className="outline-none" onClick={toggleTheme}>
-					<BsFillMoonFill
-						size={24}
-						className={`${darkMode ? "text-brandYellow" : "text-primary"}`}
+		<header className="bg-gradient-to-b dark:from-primary dark:to-secondary px-6 py-2 text-center flex items-center justify-between">
+
+			<button className="lg:hidden mr-4 woutline-none" onClick={handleSidebarVisibleState}>
+				<GiHamburgerMenu size={24} className={`${darkMode ? "text-brandYellow" : "text-primary"}`} />
+			</button>
+
+			<div className="flex justify-center items-center w-full">
+				<div className="order-2  ml-4">
+					<button className="outline-none" onClick={toggleTheme}>
+						<BsFillMoonFill
+							size={24}
+							className={`${darkMode ? "text-brandYellow" : "text-primary"}`}
+						/>
+					</button>
+				</div>
+
+				<Form className="flex flex-1 order-1" onSubmit={handleSearchQuerySubmit}>
+					<input
+						type="text"
+						placeholder="Search movie!"
+						className="dark:bg-white bg-gray-50 border-gray-800  focus:shadow-outline border dark:border-gray-300  py-2 px-4 block appearance-none leading-normal w-full md:w-1/3 md:ml-auto md:focus:w-1/2 transition-all duration-300 outline-none dark:text-gray-400 text-primary rounded-md"
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
 					/>
-				</button>
+				</Form>
 			</div>
-			<Form className="flex flex-1 w-full" onSubmit={handleSearchQuerySubmit}>
-				<input
-					type="text"
-					placeholder="Search movie!"
-					className="dark:bg-white bg-gray-50 border-gray-800  focus:shadow-outline border dark:border-gray-300  py-2 px-4 block appearance-none leading-normal w-full md:w-1/3 md:ml-auto focus:w-1/2 transition-all duration-300 outline-none dark:text-gray-400 text-primary ml-4"
-					value={searchQuery}
-					onChange={(e) => setSearchQuery(e.target.value)}
-				/>
-			</Form>
 		</header>
 	);
 };
--- app/components/layout/menuItem.tsx
+++ app/components/layout/menuItem.tsx
@@ -23,8 +23,8 @@ function MenuItem(props: MenuItemProps) {
             key={link.id + "_" + index}
             className={({ isActive }) =>
               isActive
-                ? "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white bg-brand text-white dark:bg-red-600"
-                : "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white"
+                ? "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white bg-brand text-white dark:bg-red-600 rounded-md"
+                : "flex items-center w-full px-3 py-2  hover:bg-brand hover:text-white dark:hover:bg-red-600 dark:hover:text-white rounded-md"
             }
 
           >
